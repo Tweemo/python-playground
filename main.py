bcf35ad38323e09a597dc8b30ea804d2a67706ab
Tim Liew <74855211+Tweemo@users.noreply.github.com>
2022-08-02 00:42:28 +1200

    added ability to scrap sites that have infinite scrolling

--- main.py
+++ main.py
@@ -1,56 +1,82 @@
 '''My python test playground'''
 
+import time
+import os
+import sys
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
-from datetime import datetime
-import os 
-import sys
 
 application_path = os.path.dirname(sys.executable)
 
-# Gets the current time 
+# Gets the current time
 now = datetime.now()
 # Converts time to string format
 # Creating a format that is dd/mmm/yyyy
 day_month_year = now.strftime("%d%m%Y")
 
 # Add the link to the website you want to scrape
-website = "https://www.thesun.co.uk/"
-path = "/Users/timl/Desktop/chromedriver"
+WEBSITE = "https://www.zomato.com/wellington/wellington-central-restaurants"
+PATH = "/Users/timl/Desktop/chromedriver"
 
 options = Options()
-options.headless = True
+# options.headless = True
 
-service = Service(executable_path=path)
+service = Service(executable_path=PATH)
 driver = webdriver.Chrome(service=service, options=options)
-driver.get(website)
+driver.get(WEBSITE)
 
-# Get the XPath for the container that holds the information you would like. 
-containers = driver.find_elements(by='xpath',value='//div[@class="teaser__copy-container"]/a')
+time.sleep(2)  # Allow 2 seconds for the web page to open
+SCROLL_PAUSE_TIME = 1 # You can set your own pause time. My laptop is a bit slow so I use 1 sec
+SCREEN_HEIGHT = driver.execute_script("return window.screen.height;") # get screen height
+i = 1
 
-titles = []
-subtitles = []
-links = []
+while True:
+    # scroll one screen height each time
+    driver.execute_script(f'window.scrollTo(0, {SCREEN_HEIGHT}*{i});')
+    i += 1
+    time.sleep(SCROLL_PAUSE_TIME)
+    # update scroll height each time after scrolled,
+    # as the scroll height can change after we scrolled the page
+    scroll_height = driver.execute_script("return document.body.scrollHeight;")
 
-for container in containers:
-    # Going through each individual container extracting each piece of data you 
-    # want into their respective lists
-    title = container.find_element(by='xpath', value='./h2').text
-    subtitle =  container.find_element(by='xpath', value='./p').text
-    link =  container.find_element(by='xpath', value='.').get_attribute('href')
+    # Get the XPath for the container that holds the information you would like.
+    containers = driver.find_elements(by='xpath',value='//div[@class="jumbo-tracker"]//a[2]')
 
-    titles.append(title)
-    subtitles.append(subtitle) 
-    links.append(link)
+    restaurants = []
+    links = []
+    cuisineTypes = []
+    location = []
 
-my_dict= {'title': titles, 'subtitle': subtitles, 'link': links}
+    for container in containers:
+        # Going through each individual container extracting each piece of data you
+        # want into their respective lists
+        restaurant = container.find_element(by='xpath', value='./div/h4').text
+        link =  container.find_element(by='xpath', value='.').get_attribute('href')
+        cuisine =  container.find_element(by='xpath', value='./div[2]/p').text
+
+        restaurants.append(restaurant)
+        links.append(link)
+        cuisineTypes.append(cuisine)
+        location.append('Wellington')
+
+    # Break the loop when the height we need to scroll to is larger than the total scroll height
+    if (SCREEN_HEIGHT) * i > scroll_height:
+        break
+
+
+my_dict= {'Restaurant': restaurants, 'Link': links, 'Cuisine': cuisineTypes, 'Location': location}
 
 # Converts all the lists into columns in an excel spreadsheet.
 df_headlines = pd.DataFrame(my_dict)
-file_name = f'headline-{day_month_year}.xlsx'
-final_path = os.path.join(application_path, file_name)
+FILE_NAME = 'wellington-restaurants.xlsx'
+
+final_path = os.path.join(application_path, FILE_NAME)
 df_headlines.to_excel(final_path)
+
+# Local Path
+df_headlines.to_excel('report_generation/xl_spreadsheets/' + FILE_NAME)
 
 driver.quit()
---
+++ whatsapp.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+phone_number = input('Enter Phone number')
+pywhatkit.sendwhatmsg(phone_number, "Test", 4, 43)
